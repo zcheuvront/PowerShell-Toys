6ffe2cf770b998ff96994418fd4dd3d6bfe4b81a
Zoe Cheuvront <zoe44553@gmail.com>
2022-12-21 17:52:14 -0800

    Set azure module check

--- Security/AzurePasswordGuesser.ps1
+++ Security/AzurePasswordGuesser.ps1
@@ -1,9 +1,34 @@
-# 
+# Azure password guesser. Takes in a CSV with a username and password column and makes azure login attempts using the credentials provided
+# This is very basic and doesn't take into account evasion tactics 
+
+# Example Usage: > AzurePasswordGuesser.ps1 -path C:\users\public\creds.csv
+#Requires -RunAsAdministrator
 
 [cmdLetBinding()]
 param(
-    [ValidateScript]
-        [System.IO.FileInfo] $pathtocsv
+        [ValidateScript({
+            if(-Not ($_ | Test-Path) ){
+                throw "File or folder does not exist"
+            }
+            if(-Not ($_ | Test-Path -PathType Leaf) ){
+                throw "The Path argument must be a file. Folder paths are not allowed."
+            }
+            if($_ -notmatch "(\.csv)"){
+                throw "The file specified in the path argument must be a CSV"
+            }
+            return $true 
+        })]
+        [System.IO.FileInfo]$path
 )
 
-$creds = Import-Csv $pathtocsv
+Get-Module | ForEach-Object{
+    if($_.Name -eq "PSReadLine"){
+        
+    }
+    else{
+        Write-Host -f Red "The Azure PowerShell module is missing. Please install with 'install-module azure'"
+    }
+}
+
+
+$creds = Import-Csv $path
